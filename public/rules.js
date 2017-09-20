05d37e17d7b057fb73f67aa1ea354e0cd3b31e7c
Jon <jonathan.lee.christensen@gmail.com>
2017-09-20 16:30:17 -0400

    fixed rules

--- public/rules.js
+++ public/rules.js
@@ -1,11 +1,11 @@
 //Rules will appear in order on the right side of the screen
 var rules = [
-"You may not inform Jared about the game until the game or day is over.",
+"You may not inform Jared about the game until the game is over.",
 "You may try to manipulate his actions, however, do so without arousing suspicion.",
 "Each player will have their own randomized board. Refreshing the page will not give you a new board.",
-"All squares are universal to everyone (i.e. if one person can mark something, everyone else can).",
-"It is good sportsmanship (but not required) to notify others of an event, especially to confirm subjective ones.",
-"Squares only need match the spirit of the quote rather than being exact."
+"All squares are universal to everyone (i.e. if one person can mark a square, everyone else can).",
+"It is good sportsmanship (but not required) to notify others of an event.",
+"Squares only need match the spirit of the quote rather than being exact.",
 "If necessary, Jake, Alex, and/or Jon will judge the validity of a situation.",
 "Upon winning, slack the (custom) student channel to announce your victory.",
 "The victor gets to tell Jared about the game, have someone else tell Jared, or decide what to do next."
