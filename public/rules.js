6643348ba6a3da0d3a983d09f68335ceecd53a5a
Jon <jonathan.lee.christensen@gmail.com>
2017-09-20 23:50:22 -0400

    further upgrades

--- public/rules.js
+++ public/rules.js
@@ -1,12 +1,14 @@
 //Rules will appear in order on the right side of the screen
+//The spans around Jared's name are for a secret easter egg.
 var rules = [
-"You may not inform Jared about the game until the game is over.",
-"You may try to manipulate his actions, however, do so without arousing suspicion.",
-"Each player will have their own randomized board. Refreshing the page will not give you a new board.",
-"All squares are universal to everyone (i.e. if one person can mark a square, everyone else can).",
-"It is good sportsmanship (but not required) to notify others of an event.",
-"Squares only need match the spirit of the quote rather than being exact.",
+"You may not inform <span class='secret'>Jared</span> about the game until it's over.",
+"You may try to manipulate his actions; however, do so without arousing suspicion.",
+"Each player has their own randomized board. Refreshing the page will not give you a new board.",
+"Events apply to everyone; if one person can mark a square, everyone else with that square can.",
+"It is good sportsmanship (but not required) to notify others when a square can be marked.",
+"<span class='secret'>Jared</span> need only match the spirit of a quote for it to count.",
 "If necessary, Jake, Alex, and/or Jon will judge the validity of a situation.",
-"Upon winning, slack the (custom) student channel to announce your victory.",
-"The victor gets to tell Jared about the game, have someone else tell Jared, or decide what to do next."
+"Upon winning, slack the bingo channel to announce your victory.",
+"The victor gets to tell <span class='secret'>Jared</span> about the game, have someone else tell <span class='secret'>Jared</span>, or decide what to do next.",
+"There is a secret easter egg hidden on this page."
 ]
